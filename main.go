a16d509cd04234388ef3dff791a2d6920d4c3fdc
AndreMicheletti <andreluizmtmicheletti>
2026-01-28 19:26:45 -0300

    chore: ws simple echo

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
-	"fmt"; "net/http"; "log"
+	"fmt"; "net/http"; "log"; "github.com/gorilla/websocket"
 )
-	
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hello world!")
 }
 
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	for {
+		msgType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Printf("[WEBSOCKET] message received: %s\n", p)
+		if err := conn.WriteMessage(msgType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/ws", websocketHandler)
 	log.Println("Server is listening on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
