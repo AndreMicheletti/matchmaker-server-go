178f2a33972bdde58baeadf503af94bd49b68093
AndreMicheletti <andreluizmtmicheletti>
2026-01-29 20:47:55 -0300

    chore: separate websocket logic to its file

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,17 +1,10 @@
 package api
 
 import (
-	"log"
 	"fmt"
 	"net/http"
 	"html/template"
-	"github.com/gorilla/websocket"
 )
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
 
 func (s *Server) handleHealthCheck(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
@@ -27,35 +20,4 @@ func (s *Server) handleIndex(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	tmpl.Execute(w, nil)
-}
-
-func (s *Server) handleWebsocket(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Printf("[WEBSOCKET] connection received")
-	outCh := make(chan []byte, 256)
-	defer conn.Close()
-	defer close(outCh)
-	go writeMessage(conn, outCh, websocket.TextMessage)
-	for {
-		_, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		log.Printf("[WEBSOCKET] message received: %s\n", p)
-		outCh <- p
-	}
-}
-
-func writeMessage(conn *websocket.Conn, outChannel chan []byte, msgType int) {
-	for msg := range outChannel {
-		log.Printf("[WEBSOCKET] returning message: %s\n", msg)
-		if err := conn.WriteMessage(msgType, msg); err != nil {
-			log.Println(err)
-		}
-	}
 }
--- internal/api/websocket.go
+++ internal/api/websocket.go
@@ -1,1 +1,66 @@
 package api
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	ID string
+	Conn *websocket.Conn
+	SendCh chan []byte
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func (c *Client) writePump() {
+	for msg := range c.SendCh {
+		log.Printf("[WEBSOCKET] returning message: %s\n", msg)
+		if err := c.Conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func (c *Client) readPump() {
+	for {
+		_, msg, err := c.Conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Printf("[WEBSOCKET] message received: %s\n", msg)
+		c.SendCh <- msg
+	}
+}
+
+func (s *Server) handleWebsocket(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	userID := query.Get("user_id")
+
+	if userID == "" {
+		log.Println("[WEBSOCKET] user_id não fornecido")
+		return
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Printf("[WEBSOCKET] connection received for user %s", userID)
+
+	c := Client{}
+	c.ID = userID
+	c.SendCh = make(chan []byte, 256)
+	c.Conn = conn
+
+	defer close(c.SendCh)
+	defer conn.Close()
+	go c.writePump()
+	c.readPump()
+}
